96b5c10f7aef56584fd104945cacb08cdecdace1
shubha96ngi <shubha.96n.gi@gmail.com>
2024-06-18 08:39:06 +0530

    Update gauss_kernel.py

--- gauss_kernel.py
+++ gauss_kernel.py
@@ -2,20 +2,6 @@ import numpy as np
 from scipy.stats import norm
 from scipy.signal import convolve2d
 
-def minmax(data, newmax, newmin):
-    arr = np.zeros_like(data)
-    data_min = np.min(data, axis=0)
-    data_max = np.max(data, axis=0)
-    data_range = data_max - data_min
-    
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            arr[i, j] = (data[i, j] - data_min[j]) / data_range[j]
-            range2 = newmax - newmin
-            arr[i, j] = arr[i, j] * range2 + newmin
-    
-    return arr
-
 def gauss_kernel(inp_for_sys, cnt_MF):
     x = np.arange(-360, 360.1, 0.1)  # Equivalent to -360:0.1:360 in Matlab
     mf_spiketime = []
