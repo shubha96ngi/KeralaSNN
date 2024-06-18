d57ca3ac80e1b1d597806bf11f6a80c9a3905fd9
shubha96ngi <shubha.96n.gi@gmail.com>
2024-06-18 08:37:09 +0530

    Create gauss_kernel.py

---
+++ gauss_kernel.py
@@ -0,0 +1,67 @@
+import numpy as np
+from scipy.stats import norm
+from scipy.signal import convolve2d
+
+def minmax(data, newmax, newmin):
+    arr = np.zeros_like(data)
+    data_min = np.min(data, axis=0)
+    data_max = np.max(data, axis=0)
+    data_range = data_max - data_min
+    
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            arr[i, j] = (data[i, j] - data_min[j]) / data_range[j]
+            range2 = newmax - newmin
+            arr[i, j] = arr[i, j] * range2 + newmin
+    
+    return arr
+
+def gauss_kernel(inp_for_sys, cnt_MF):
+    x = np.arange(-360, 360.1, 0.1)  # Equivalent to -360:0.1:360 in Matlab
+    mf_spiketime = []
+
+    # Normalizing the features using the minmax function
+    inp_for_sys_norm = minmax(inp_for_sys, 255, 20)
+
+    for loop_j in range(inp_for_sys_norm.shape[0]):
+        stor_sum_val = []
+        mf_spike = [[] for _ in range(inp_for_sys_norm.shape[1])]
+
+        for i in range(inp_for_sys_norm.shape[1]):
+            norm = norm.pdf(x, loc=inp_for_sys_norm[loop_j, i], scale=10)
+            norm1 = convolve2d(norm.reshape(1, -1), inp_for_sys_norm[loop_j, i].reshape(1, 1), mode='same')
+            norm1 = (norm1 - np.min(norm1)) / (np.max(norm1) - np.min(norm1))
+
+            datapoints = np.zeros(cnt_MF)
+            for j in range(cnt_MF):
+                datapoints[j] = createneuron(inp_for_sys_norm[loop_j, i], cnt_MF)
+
+            wt = (inp_for_sys_norm[loop_j, i] / 3) * len(datapoints)
+
+            l_dp = np.isclose(np.round(x, 1), np.round(datapoints, 1).reshape(1, -1))
+            find_dp = np.where(l_dp)[1]
+            stor_sum = wt * norm1[0, find_dp]
+
+            stor_sum_val.append(stor_sum)
+            new_storing_mat = np.array(stor_sum_val)
+
+            # Code to find the spiketime of neurons all features
+            for j in range(new_storing_mat.shape[1]):
+                mfspike = EulerAdEx(new_storing_mat[i, j])
+                mf_spike[i].append(mfspike)
+
+        celength = [len(row) for row in mf_spike]
+        m = max(celength)
+
+        spike_positions = [[] for _ in range(inp_for_sys_norm.shape[1])]
+        for i in range(inp_for_sys_norm.shape[1]):
+            for j in range(len(mf_spike[i])):
+                if len(mf_spike[i][j]) < m:
+                    temp_zero = np.zeros(m - len(mf_spike[i][j]))
+                    mf_spike[i][j] = np.concatenate((mf_spike[i][j], temp_zero))
+
+                spike_positions[i].append(mf_spike[i][j])
+
+        mf_spiketime.append(spiktime2matrix(spike_positions))
+
+    return mf_spiketime
